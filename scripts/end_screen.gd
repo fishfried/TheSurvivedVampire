df242291542bb16f616c422c7551b11a4b50249b
fishfried <104720816+fishfried@users.noreply.github.com>
2025-09-21 15:15:36 +0800

    Add files via upload

---
+++ scripts/end_screen.gd
@@ -0,0 +1,37 @@
+extends CanvasLayer
+
+@onready var panel_container = $%PanelContainer
+
+func _ready():
+	panel_container.pivot_offset = panel_container.size/2
+	var tween = create_tween()
+	tween.tween_property(panel_container,"scale",Vector2.ZERO,0)
+	tween.tween_property(panel_container,"scale",Vector2.ONE,.3)\
+	.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BACK)
+	
+	get_tree().paused = true
+	$%ContinueButton.pressed.connect(on_continue_button_pressed)
+	$%QuitButton.pressed.connect(on_quit_button_pressed)
+	
+func set_defeat():
+	$%TitleLabel.text = "寄咯"
+	$%DescriptionLabel.text = "哎你怎么死了"
+	play_jingle(true)
+
+func play_jingle(defeat:bool = false):
+	if defeat:
+		$DefeatStreamPlayer.play()
+	else :
+		$VictoryStreamPlayer.play()
+
+func on_continue_button_pressed():
+	ScreenTransition.transition()
+	await ScreenTransition.transitioned_halfway
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://scenes/UI/meta_menu.tscn")
+
+func on_quit_button_pressed():
+	get_tree().paused = false
+	ScreenTransition.transition_to_scene("res://scenes/UI/main_menu.tscn")
+	await ScreenTransition.transitioned_halfway
+	
